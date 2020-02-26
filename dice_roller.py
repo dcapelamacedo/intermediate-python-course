db513dba370e2d97fd0eadafeb051814c3afa879
dcapelamacedo <59672607+dcapelamacedo@users.noreply.github.com>
2020-02-26 16:14:09 +0000

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,12 @@
 import random
 
+times_rolled = 0
+
 def main():
   roll = random.randint(1,6)
+  times_rolled += roll
   print(f'You rolled a {roll}')
-  
+print (f'You have rolled {times_rolled}  
 if __name__== "__main__":
   main()
   
